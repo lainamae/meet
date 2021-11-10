128139d2e9eaaad324929c0c23d52d9e84bf58be
lainamae <64325945+lainamae@users.noreply.github.com>
2021-11-10 17:05:15 -0500

    styling update

--- src/EventList.js
+++ src/EventList.js
@@ -2,15 +2,19 @@
 
 import React, { Component } from 'react';
 import Event from './Event';
+import {Col} from 'react-bootstrap'
+
 class EventList extends Component {
   render() {
     const { events } = this.props;
     return (
       <ul className="EventList d-flex flex-row">
  {events.map(event =>
-        <li className="col"key={event.id}>
+        <Col md={4} key={event.id}>
+        <li>
           <Event event={event} />
         </li>
+        </Col>
       )}
       </ul>
     );
